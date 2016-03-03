0e5171528b3c00715721469d5f3ff28af4974991
comfortcode <nechamabgoldman@gmail.com>
2016-03-03 21:16:13 +0000

    automated rake to delete expired items (using whenever gem)

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ require 'faker'
    Item.create!(
      user:   users.sample,
      name:  Faker::Lorem.sentence,
-     created_at: Faker::Time.between(DateTime.now - 6, DateTime.now)
+     created_at: Faker::Time.between(DateTime.now - 9, DateTime.now)
    )
  end
 items = Item.all
