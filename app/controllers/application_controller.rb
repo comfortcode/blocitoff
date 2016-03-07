4ac4242369df8088844f234546f7e6d54e89fffd
comfortcode <nechamabgoldman@gmail.com>
2016-03-07 15:49:52 +0000

    User Experience tweaks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   # When user signs in, redirect to their registry
     def after_sign_in_path_for(resource)
-      users_show_path
+      list_path
     end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :user
+  validates :name, presence: true
 
     def days_left
       7 - (DateTime.now.to_date - created_at.to_date).to_i
